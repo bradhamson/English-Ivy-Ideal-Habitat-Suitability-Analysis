60a1030e5fe879714fa94147a7d223302c6472a0
bradhamson <brad.hamson@gmail.com>
2016-05-04 21:29:56 -0400

    automate analysis

---
+++ scripts/scripts.rst
@@ -0,0 +1,50 @@
+==================
+Automated Analysis
+==================
+
+--------
+Contents
+--------
+* json/remap.json
+* pickles/algos.p
+* reproject.py
+* suitability_analysis.py
+
+-----------
+Description
+-----------
+This site suitability analysis has been completely automated using
+the above scripts and resources.
+
+The `reproject.py` script was initially used to reproject all of the 
+source data to CRS: 102387. This is no longer needed since the 
+`suitability_analysis.py` script includes this functionality.
+
+The `suitability_analysis.py` script has been developed to automate
+the data management and analysis portions of this project. Reclassification is performed my reading from the `remap.json` file and feeding the values into ESRI's `Remap` objects instead of using their infotable methods. Fuzzy Membership algorithms are stored as pickled objects in the `algos.p` file and used in a similar manner to apply Fuzzy Membership to the raster datasets. Make sure to keep this directory structure when running the analysis.
+
+Source data is provided as shapefiles in the `Data/Sources` directory.
+
+The script performs the following actions:
+
+Data Management
+* Creates an empty `working_data.gdb` file geodatabase
+* Loads the data in `Data/Sources` into the fgdb
+* Reprojects the data to the correct CRS
+* Removes the old unprojected feature classes
+
+Analysis
+* Creates distance surfaces for the Streets and GreenSpaces features
+* Rasterizes the Soils feature class
+* Generates an Aspect Surface from the DEM
+* Reclassifies the raster datasets
+* Applies the correct Fuzzy Membership algorithms to the raster datasets
+* Performs the Fuzzy Overlay Analysis using the rasters in the Fuzzy Membership set
+
+---
+Use
+---
+To use the script first download the Baltimore 1 meter DEM `here <https://www.dropbox.com/s/j7x2ips8donvpd2/BaltimoreCity_DEM_2015_0.7m.7z?dl=0>`_
+and place it in the `Data/Sources` directory.
+
+Then run the script from your shell and enter the source path when prompted.
---
+++ scripts/suitability_analysis.py
@@ -0,0 +1,139 @@
+import os
+import json
+import pickle
+import arcpy
+from arcpy.sa import (EucDistance, 
+	Aspect, 
+	RemapRange, 
+	RemapValue, 
+	Reclassify,
+	FuzzyMembership,
+	FuzzyNear,
+	FuzzySmall,
+	FuzzyOverlay)
+
+def list_objects():
+	objects = arcpy.ListFeatureClasses()
+	rasters = arcpy.ListRasters()
+	for raster in rasters:
+		objects.append(raster)
+	return objects
+
+def switch_workspace(src, *args):
+	if arcpy.env.workspace == src:
+		arcpy.env.workspace = args[0]
+	if not arcpy.env.workspace:
+		arcpy.env.workspace = src
+
+def create_output_gdb(src):
+	output_dir = os.path.dirname(src)
+	dest = os.path.join(output_dir, 'working_data.gdb')
+	arcpy.CreateFileGDB_management(output_dir, 'working_data.gdb')
+	add_to_gdb(src, dest)
+	reproject()
+
+def add_to_gdb(src, dest):
+	switch_workspace(src)
+	vector = arcpy.ListFeatureClasses()
+	raster = arcpy.ListRasters()
+	for fc in raster:
+		arcpy.RasterToGeodatabase_conversion(fc, dest)
+	for fc in vector:
+		arcpy.CopyFeatures_management(fc, os.path.join(dest, 
+			fc.split('.')[0]))
+	switch_workspace(src, dest)
+
+def clean_up():
+	fcs = arcpy.ListFeatureClasses()
+	for fc in fcs:
+		desc = arcpy.Describe(fc)
+		if not desc.spatialReference.factoryCode == 102387:
+			arcpy.Delete_management(fc)
+		if fc.endswith('_projected'):
+			arcpy.Rename_management(fc, fc.split('_')[0])
+
+def reproject():
+	out_crs = arcpy.SpatialReference(102387)
+	fcs = arcpy.ListFeatureClasses()
+	for fc in fcs:
+		desc = arcpy.Describe(fc)
+		if not desc.spatialReference.factoryCode == 102387:
+			out_fc = '{}_projected'.format(fc)
+			arcpy.Project_management(fc, out_fc, out_crs)
+	clean_up()
+
+def distance(fc):
+	output = os.path.join(arcpy.env.workspace, '{}_dist'.format(fc))
+	distance = EucDistance(fc, "#", 63.9370612000003)
+	arcpy.CopyRaster_management(distance, output, '', '#', '', 'NONE',
+		'NONE', '32_BIT_UNSIGNED')
+
+def aspect(fc):
+	output = os.path.join(arcpy.env.workspace, 'Aspect')
+	desc = arcpy.Describe(fc)
+	if desc.dataType == 'RasterDataset':
+		aspect = Aspect(fc)
+		aspect.save(output)
+
+def rasterize(fc):
+	output = os.path.join(arcpy.env.workspace, 'Soils')
+	arcpy.FeatureToRaster_conversion(fc, 'Drainage', output, 63.3278848987445)
+
+def reclassify(fcs):
+	remap = json.load(open('json/remap.json', 'r+'))
+	for key, value in remap.iteritems():
+		for fc in fcs:
+			output = os.path.join(arcpy.env.workspace, 
+				'{}_reclass'.format(fc))
+			if key != 'Soils' and key in fc:
+				remap_range = RemapRange(value)
+				rc = Reclassify(fc, 'Value', remap_range, 'NODATA')
+				rc.save(output)
+			if key == 'Soils' and key == fc:
+				remap_value = RemapValue(value)
+				rc = Reclassify(fc, 'Value', remap_value, 'NODATA')
+				rc.save(output)
+
+def add_members(fcs):
+	algos = pickle.load(open('pickles/algos.p', 'rb'))
+	for fc in fcs:
+		if 'Streets' in fc:
+			output = os.path.join(arcpy.env.workspace, '{}_fuzzy'.format(fc))
+			fm = FuzzyMembership(fc, algos['linear'])
+			fm.save(output)
+		else:
+			output = os.path.join(arcpy.env.workspace, '{}_fuzzy'.format(fc))
+			fm = FuzzyMembership(fc, algos['small'])
+			fm.save(output)
+	members = [m for m in arcpy.ListRasters() if m.endswith('_fuzzy')]
+	return members
+	
+def prepare_data():
+	fcs = list_objects()
+	for fc in fcs:
+		if 'Streets' in fc or 'GreenSpaces' in fc: 
+			distance(fc)
+		if 'Soils' in fc:
+			rasterize(fc)
+		aspect(fc)
+	reclassify(arcpy.ListRasters())
+	data = [i for i in arcpy.ListRasters() if i.endswith('_reclass')]
+	return data
+
+def overlay():
+	arcpy.env.compression = 'NONE'
+	output = os.path.join(arcpy.env.workspace, 'SiteSuitability_Results')
+	data = prepare_data()
+	members = add_members(data)
+	final_output = FuzzyOverlay(members, 'AND')
+	final_output.save(output)
+
+def main():
+	arcpy.CheckOutExtension('spatial')
+	source = raw_input('Path to Source Data Directory: ')
+	create_output_gdb(source)
+	overlay()
+	arcpy.CheckInExtension('spatial')
+
+if __name__ == '__main__':
+	main()
